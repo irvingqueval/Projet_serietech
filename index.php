b9113aa4272decb341285bc79323cc4c61840faa
Irving Queval <irvingqueval@gmail.com>
2024-07-16 10:21:49 +0200

    main page update

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
 include "header.php";
 
+$query = "SELECT * FROM serie";
+$statement = $pdo->query($query);
+$series = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($series as $serie){
+    ?>
+    <div class="card mb-3" style="max-width: 100;">
+  <div class="row g-0">
+    <div class="col-md-4">
+        <img src="<?=$serie["image_url"] ?>" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title" style="font-size: 30px;"><?=$serie["name"]?></h5>
+        <p class="card-text"><?=$serie["synopsis"]?></p>
+        <button type="button" class="btn btn-outline-danger">Delete</button>
+      </div>
+    </div>
+  </div>
+</div>
+    <?php
+}
